1fa8e79a954906117ce554126959c34312c03ae1
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-11-03 21:56:47 -0300

    Projeto mobile com layout finalizado

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -11,6 +11,9 @@ import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/styles/theme";
 import { SignIn } from "./src/screens/SignIn";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
+import { Pools } from "./src/screens/Pools";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +30,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
